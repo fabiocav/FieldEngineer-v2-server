e9a6188e4beb4ea85734573621869678a01d3f7a
fabiocav <facaval@microsoft.com>
2015-05-11 15:04:56 -0700

    Separated the storage controller logic

--- FieldEngineerLiteService/Controllers/JobController.cs
+++ FieldEngineerLiteService/Controllers/JobController.cs
@@ -86,115 +86,115 @@ namespace FieldEngineerLiteService.Controllers
             return DeleteAsync(id);
         }
 
-        [HttpPost]
-        [Route("tables/Job/{id}/StorageToken")]
-        public async Task<HttpResponseMessage> PostStorageTokenRequest(string id, [FromBody]StorageTokenRequest value)
-        {
-            ServiceUser user = this.User as ServiceUser;
+        //[HttpPost]
+        //[Route("tables/Job/{id}/StorageToken")]
+        //public async Task<HttpResponseMessage> PostStorageTokenRequest(string id, [FromBody]StorageTokenRequest value)
+        //{
+        //    ServiceUser user = this.User as ServiceUser;
 
-            if (user == null || !IsTokenRequestValid(value, user))
-            {
-                this.Unauthorized();
-            }
+        //    if (user == null || !IsTokenRequestValid(value, user))
+        //    {
+        //        this.Unauthorized();
+        //    }
 
-            StorageToken token = await this.GetAccessTokenAsync(value, id, user);
+        //    StorageToken token = await this.GetAccessTokenAsync(value, id, user);
 
-            return Request.CreateResponse(token);
-        }
+        //    return Request.CreateResponse(token);
+        //}
 
-        // Get the files associated with this record
-        [HttpGet]
-        [Route("tables/Job/{id}/MobileServiceFiles")]
-        public async Task<HttpResponseMessage> GetFiles(string id)
-        {
-            ServiceUser user = this.User as ServiceUser;
+        //// Get the files associated with this record
+        //[HttpGet]
+        //[Route("tables/Job/{id}/MobileServiceFiles")]
+        //public async Task<HttpResponseMessage> GetFiles(string id)
+        //{
+        //    ServiceUser user = this.User as ServiceUser;
 
-            // Validate user and request
+        //    // Validate user and request
 
-            string containerName = GetContainerNameForRequest(id);
-            CloudBlobContainer container = GetRecordContainer(containerName);
+        //    string containerName = GetContainerNameForRequest(id);
+        //    CloudBlobContainer container = GetRecordContainer(containerName);
 
-            IEnumerable<IListBlobItem> blobs = container.ListBlobs(blobListingDetails: BlobListingDetails.Metadata);
-            IEnumerable<MobileServiceFile> files = blobs.OfType<CloudBlockBlob>().Select(b => MobileServiceFile.FromBlobItem(b, "Job", id));
-            return Request.CreateResponse(files);
-        }
+        //    IEnumerable<IListBlobItem> blobs = container.ListBlobs(blobListingDetails: BlobListingDetails.Metadata);
+        //    IEnumerable<MobileServiceFile> files = blobs.OfType<CloudBlockBlob>().Select(b => MobileServiceFile.FromBlobItem(b, "Job", id));
+        //    return Request.CreateResponse(files);
+        //}
 
-        [HttpDelete]
-        [Route("tables/Job/{id}/MobileServiceFiles/{name}")]
-        public async Task GetFiles(string id, string name)
-        {
-            ServiceUser user = this.User as ServiceUser;
+        //[HttpDelete]
+        //[Route("tables/Job/{id}/MobileServiceFiles/{name}")]
+        //public async Task GetFiles(string id, string name)
+        //{
+        //    ServiceUser user = this.User as ServiceUser;
 
-            // Validate user and request
+        //    // Validate user and request
 
-            string containerName = GetContainerNameForRequest(id);
-            CloudBlobContainer container = GetRecordContainer(containerName);
+        //    string containerName = GetContainerNameForRequest(id);
+        //    CloudBlobContainer container = GetRecordContainer(containerName);
 
-            CloudBlob blob = container.GetBlobReference(name);
-            await blob.DeleteIfExistsAsync();
-        }
+        //    CloudBlob blob = container.GetBlobReference(name);
+        //    await blob.DeleteIfExistsAsync();
+        //}
 
-        protected virtual bool IsTokenRequestValid(StorageTokenRequest request, ServiceUser user)
-        {
-            return true;
-        }
+        //protected virtual bool IsTokenRequestValid(StorageTokenRequest request, ServiceUser user)
+        //{
+        //    return true;
+        //}
 
 
-        public async Task<StorageToken> GetAccessTokenAsync(StorageTokenRequest request, string entityId, ServiceUser user)
-        {
-            if (request == null)
-            {
-                throw new ArgumentNullException("request");
-            }
+        //public async Task<StorageToken> GetAccessTokenAsync(StorageTokenRequest request, string entityId, ServiceUser user)
+        //{
+        //    if (request == null)
+        //    {
+        //        throw new ArgumentNullException("request");
+        //    }
 
-            if (user == null)
-            {
-                throw new ArgumentNullException("user");
-            }
+        //    if (user == null)
+        //    {
+        //        throw new ArgumentNullException("user");
+        //    }
 
-            string containerName = this.GetContainerNameForRequest(entityId);
-            CloudBlobContainer container = GetRecordContainer(containerName);
+        //    string containerName = this.GetContainerNameForRequest(entityId);
+        //    CloudBlobContainer container = GetRecordContainer(containerName);
 
-            var constraints = new SharedAccessBlobPolicy();
-            constraints.SharedAccessExpiryTime = DateTime.UtcNow.AddHours(1);
-            constraints.Permissions = GetBlobAccessPermissions(request.Permissions);
+        //    var constraints = new SharedAccessBlobPolicy();
+        //    constraints.SharedAccessExpiryTime = DateTime.UtcNow.AddHours(1);
+        //    constraints.Permissions = GetBlobAccessPermissions(request.Permissions);
 
-            string containerToken = await Task.Run(() => container.GetSharedAccessSignature(constraints));
+        //    string containerToken = await Task.Run(() => container.GetSharedAccessSignature(constraints));
 
-            var storageToken = new StorageToken();
-            storageToken.Permissions = request.Permissions;
-            storageToken.RawToken = container.Uri + containerToken;
-            storageToken.EntityId = entityId;
+        //    var storageToken = new StorageToken();
+        //    storageToken.Permissions = request.Permissions;
+        //    storageToken.RawToken = container.Uri + containerToken;
+        //    storageToken.EntityId = entityId;
 
-            return storageToken;
-        }
+        //    return storageToken;
+        //}
 
-        private CloudBlobContainer GetRecordContainer(string containerName)
-        {
-            CloudStorageAccount account = CloudStorageAccount.Parse(this.Services.Settings.Connections["mS_AzureStorageAccountConnectionString"].ConnectionString);
-            CloudBlobClient blobClient = account.CreateCloudBlobClient();
+        //private CloudBlobContainer GetRecordContainer(string containerName)
+        //{
+        //    CloudStorageAccount account = CloudStorageAccount.Parse(this.Services.Settings.Connections["mS_AzureStorageAccountConnectionString"].ConnectionString);
+        //    CloudBlobClient blobClient = account.CreateCloudBlobClient();
 
-            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
-            container.CreateIfNotExists();
-            return container;
-        }
+        //    CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+        //    container.CreateIfNotExists();
+        //    return container;
+        //}
 
-        private string GetContainerNameForRequest(string entityId)
-        {
-            // - Default format: entityname-recordid
-            // - Expose a mechanism to allow developers to change how names are resolved.
-            // - Need validation to ensure we have a valid container name: https://msdn.microsoft.com/en-us/library/azure/dd135715.aspx
+        //private string GetContainerNameForRequest(string entityId)
+        //{
+        //    // - Default format: entityname-recordid
+        //    // - Expose a mechanism to allow developers to change how names are resolved.
+        //    // - Need validation to ensure we have a valid container name: https://msdn.microsoft.com/en-us/library/azure/dd135715.aspx
 
-            return string.Format("job-{0}", entityId);
-        }
+        //    return string.Format("job-{0}", entityId);
+        //}
 
-        private SharedAccessBlobPermissions GetBlobAccessPermissions(StoragePermissions storagePermissions)
-        {
-            SharedAccessBlobPermissions permissions = storagePermissionsMapping
-                .Aggregate(SharedAccessBlobPermissions.None, (a, kvp) => (storagePermissions & kvp.Key) == kvp.Key ? a |= kvp.Value : a);
+        //private SharedAccessBlobPermissions GetBlobAccessPermissions(StoragePermissions storagePermissions)
+        //{
+        //    SharedAccessBlobPermissions permissions = storagePermissionsMapping
+        //        .Aggregate(SharedAccessBlobPermissions.None, (a, kvp) => (storagePermissions & kvp.Key) == kvp.Key ? a |= kvp.Value : a);
 
-            return permissions;
-        }
+        //    return permissions;
+        //}
     }
 
     public class MobileServiceFile
---
+++ FieldEngineerLiteService/Controllers/JobStorageController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using System.Text;
+using System.Threading.Tasks;
+using FieldEngineerLiteService.DataObjects;
+using FieldEngineerLiteService.Files;
+
+
+namespace FieldEngineerLiteService.Controllers
+{
+    public class JobStorageController : StorageController<Job>
+    {
+        [HttpPost]
+        [Route("tables/Job/{id}/StorageToken")]
+        public override Task<HttpResponseMessage> PostStorageTokenRequest(string id, StorageTokenRequest value)
+        {
+            return base.PostStorageTokenRequest(id, value);
+        }
+
+        // Get the files associated with this record
+        [HttpGet]
+        [Route("tables/Job/{id}/MobileServiceFiles")]
+        public override Task<HttpResponseMessage> GetFiles(string id)
+        {
+            return base.GetFiles(id);
+        }
+
+        [HttpDelete]
+        [Route("tables/Job/{id}/MobileServiceFiles/{name}")]
+        public override Task DeleteFile(string id, string name)
+        {
+            return base.DeleteFile(id, name);
+        }
+
+    }
+}
